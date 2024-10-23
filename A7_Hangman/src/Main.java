39e4c15462c4ac45c5bdc65fed0bd69240f172f4
Red__Jack <126083814+Red05Jack@users.noreply.github.com>
2024-10-23 08:45:31 +0200

    Hangman function testing

--- A7_Hangman/src/Main.java
+++ A7_Hangman/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        Hangman hangmanGame = new Hangman("elephant", 6);
+
+        System.out.println(hangmanGame.getRemainingAttempts());
+
+        System.out.println(hangmanGame.guessLetter('e'));
+        System.out.println(hangmanGame.guessLetter('x'));
+
+        System.out.println(hangmanGame.getCurrentWordState());
+
+        System.out.println(hangmanGame.isGameWon());
+        System.out.println(hangmanGame.isGameOver());
+        System.out.println(hangmanGame.getRemainingAttempts());
+        System.out.println(hangmanGame.getWordToGuess());
+
+
     }
+
 }
